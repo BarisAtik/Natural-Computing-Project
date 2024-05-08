01d7257b08638bf25172a9dd077faabead6a3467
Cagri <baris.atik@student.ru.nl>
2024-05-08 14:15:34 +0200

    NSGAII af

--- scripts/NSGA2.py
+++ scripts/NSGA2.py
@@ -27,7 +27,10 @@ class NSGA2:
         self.max_pollution = sum([node.pollution for node in self.repr.nodes.values()])
         self.max_depth = 1000
 
-    def generate_route(self, start_node=None, old_route=[]):
+    def generate_route(self, start_node=None, old_route=[], depth=1):
+        if depth > self.max_depth:
+            return []
+
         route = [start_node] if start_node else [self.start_node]
         while route[-1] != self.end_node:
             forward_nodes = [
@@ -36,16 +39,17 @@ class NSGA2:
                 if node not in route and node not in old_route
             ]
             if forward_nodes == []:
-                return self.generate_route(start_node, old_route)
+                return self.generate_route(start_node, old_route, depth + 1)
             next_node = random.choice(forward_nodes)
             route.append(next_node)
         return route
 
     def init_population(self):
         initial_population = []
-        for _ in range(self.population_size):
+        while len(initial_population) < self.population_size:
             route = self.generate_route()
-            initial_population.append(route)
+            if route != []:
+                initial_population.append(route)
         return initial_population
 
     def calculate_fitness(self, route):
@@ -55,11 +59,19 @@ class NSGA2:
                 self.repr.nodes[route[i]].coordinates,
                 self.repr.nodes[route[i + 1]].coordinates,
             )
-        total_polution = 0
-        # Decide a way to calculate the total polution
-        total_time = 0
-        # For each distance, calculate the time it takes to travel that distance according to speed limit
-        return 1 * total_distance + 1 * total_polution + 1 * total_time
+        total_traffic = sum([self.repr.nodes[node].traffic for node in route])
+        total_pollution = sum([self.repr.nodes[node].pollution for node in route])
+
+        normalized_distance = total_distance / self.max_distance
+        normalized_traffic = total_traffic / self.max_traffic
+        normalized_pollution = total_pollution / self.max_pollution
+
+        fitness = (
+            self.weights[0] * normalized_distance
+            + self.weights[1] * normalized_traffic
+            + self.weights[2] * normalized_pollution
+        )
+        return fitness, total_distance, total_traffic, total_pollution
 
     def fast_non_dominated_sort(self, population, group_size):
         frontiers = []
@@ -70,8 +82,8 @@ class NSGA2:
 
         for i, p in enumerate(population):
             for q in population:
-                p_fitness = self.calculate_fitness(p)
-                q_fitness = self.calculate_fitness(q)
+                p_fitness = self.calculate_fitness(p)[0]
+                q_fitness = self.calculate_fitness(q)[0]
                 if p_fitness < q_fitness:
                     dominated_solutions[i].append(q)
                 elif p_fitness > q_fitness:
@@ -179,9 +191,10 @@ class NSGA2:
     def mutation(self, route):
         mutation_point = random.choice(route[1:-1])
         index = route.index(mutation_point)
-        mutated_route = route[:index] + self.generate_route(
-            mutation_point, route[:index]
-        )
+        new_part = self.generate_route(mutation_point, route[:index])
+        if new_part == []:
+            return route
+        mutated_route = route[:index] + new_part
         return mutated_route
 
     def create_offspring(self, parents, p_crossover, p_mutation):
@@ -211,48 +224,110 @@ class NSGA2:
         best_fitness,
         nr_generations,
         map_name,
+        ylabel,
         show_results=True,
         save_name=None,
     ):
         plt.figure(figsize=(10, 7))
-        plt.plot(range(nr_generations + 1), avg_fitness, label="Average for NSGA2")
-        plt.plot(range(nr_generations + 1), best_fitness, label="Best for NSGA2")
+        plt.plot(range(nr_generations + 1), avg_fitness, label="Average for HEADRPP")
+        plt.plot(range(nr_generations + 1), best_fitness, label="Best for HEADRPP")
         plt.xlim([0, nr_generations])
         plt.xlabel("Time (generations)")
-        plt.ylabel("Route distance")
+        plt.ylabel(ylabel)
         plt.title(
-            f"Fitness of population over time for the HEADRPP algorithm applied on a map of {map_name}"
+            f"{ylabel} of population over time for the HEADRPP algorithm applied on a map of {map_name}"
         )
         plt.legend()
         if save_name:
-            plt.savefig(save_name)
+            plt.savefig(save_name + f"_headrpp_{ylabel.replace(' ', '_').lower()}.png")
         if show_results:
             plt.show()
         plt.close()
 
     def run_algorithm(self, show_results=True, save_name=None):
         population = self.init_population()
-        avg_fitness = [np.mean([self.calculate_fitness(route) for route in population])]
-        best_fitness = [np.min([self.calculate_fitness(route) for route in population])]
+
+        fitness_values = np.array(
+            [self.calculate_fitness(route) for route in population]
+        )
+
+        avg_fitness, best_fitness = [np.mean(fitness_values[:, 0])], [
+            np.min(fitness_values[:, 0])
+        ]
+        avg_distance, best_distance = [np.mean(fitness_values[:, 1])], [
+            np.min(fitness_values[:, 1])
+        ]
+        avg_traffic, best_traffic = [np.mean(fitness_values[:, 2])], [
+            np.min(fitness_values[:, 2])
+        ]
+        avg_pollution, best_pollution = [np.mean(fitness_values[:, 3])], [
+            np.min(fitness_values[:, 3])
+        ]
 
         for _ in tqdm(range(self.nr_generations)):
-            # print(self.fast_non_dominated_sort(population, 2))
-            sorted_population = self.fast_non_dominated_sort(population, 2)
-            fittest_parents = self.crowding_distance_selection(sorted_population, 2)
+            fast_non_dominated_sort = self.fast_non_dominated_sort(population, 2)
+            fittest_parents = self.crowding_distance_selection(fast_non_dominated_sort, 2)
             population = self.create_offspring(fittest_parents, 0.8, 0.3)
-            avg_fitness.append(
-                np.mean([self.calculate_fitness(route) for route in population])
+            fitness_values = np.array(
+                [self.calculate_fitness(route) for route in population]
             )
-            best_fitness.append(
-                np.min([self.calculate_fitness(route) for route in population])
-            )
+            avg_fitness.append(np.mean(fitness_values[:, 0]))
+            best_fitness.append(np.min(fitness_values[:, 0]))
+            avg_distance.append(np.mean(fitness_values[:, 1]))
+            best_distance.append(np.min(fitness_values[:, 1]))
+            avg_traffic.append(np.mean(fitness_values[:, 2]))
+            best_traffic.append(np.min(fitness_values[:, 2]))
+            avg_pollution.append(np.mean(fitness_values[:, 3]))
+            best_pollution.append(np.min(fitness_values[:, 3]))
+
+        best_route = population[np.argmin(fitness_values[:, 0])]
 
         self.plot_results(
             avg_fitness,
             best_fitness,
             self.nr_generations,
             self.repr.map_name,
+            "Fitness",
             show_results,
             save_name,
         )
-        return population, avg_fitness, best_fitness
+        self.plot_results(
+            avg_distance,
+            best_distance,
+            self.nr_generations,
+            self.repr.map_name,
+            "Route distance",
+            show_results,
+            save_name,
+        )
+        self.plot_results(
+            avg_traffic,
+            best_traffic,
+            self.nr_generations,
+            self.repr.map_name,
+            "Route traffic",
+            show_results,
+            save_name,
+        )
+        self.plot_results(
+            avg_pollution,
+            best_pollution,
+            self.nr_generations,
+            self.repr.map_name,
+            "Route pollution",
+            show_results,
+            save_name,
+        )
+
+        return (
+            population,
+            best_route,
+            avg_fitness,
+            best_fitness,
+            avg_distance,
+            best_distance,
+            avg_traffic,
+            best_traffic,
+            avg_pollution,
+            best_pollution,
+        )
